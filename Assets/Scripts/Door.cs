2adade618f9f55ce1c75c0968d521b1b65a3b80d
Riquegamer <hiquehique12348@gmail.com>
2024-11-05 18:56:31 -0300

    Final Map Release

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -5,19 +5,20 @@ public class Door : MonoBehaviour
     #region Variables
 
     [Header("Properties")]
-    [SerializeField] private bool _left; // Define se a porta abre
+    [SerializeField] private bool _left; // Define se a porta abre para esquerda
     [SerializeField] private float openSpeed = 2.0f; // Velocidade de abertura da porta
     [SerializeField] private float openHeight = 2.0f; // Altura que a porta deve subir
 
     [Header("Model")]
-    [SerializeField] private Transform doorModel; // Modelo da porta que ser� movido
+    [SerializeField] private Transform doorModel; // Modelo da porta que será movido
 
     [Header("State")]
     private bool isOpen = false; // Estado da porta (aberta ou fechada)
+    public bool lockedByPassword = true; // Porta bloqueada até a senha ser correta
 
     [Header("Positions")]
-    private Vector3 initialPosition; // Posi��o inicial da porta
-    private Vector3 targetPosition; // Posi��o alvo da porta quando aberta
+    private Vector3 initialPosition; // Posição inicial da porta
+    private Vector3 targetPosition; // Posição alvo da porta quando aberta
 
     #endregion
 
@@ -27,16 +28,16 @@ public class Door : MonoBehaviour
         initialPosition = doorModel.position;
         if (!_left) 
         { 
-            targetPosition = initialPosition + Vector3.up * openHeight; // Define a posi��o alvo da porta
+            targetPosition = initialPosition + Vector3.up * openHeight; // Define a posição alvo da porta
         }else
         {
-            targetPosition = initialPosition + Vector3.left * openHeight; // Define a posi��o alvo da porta
+            targetPosition = initialPosition + Vector3.left * openHeight; // Define a posição alvo da porta
         }
     }
 
     private void Update()
     {
-        // Verifica se a porta est� aberta e move a porta para a posi��o alvo
+        // Verifica se a porta está aberta e move a porta para a posição alvo
         if (isOpen)
         {
             // Move a porta suavemente para cima
@@ -44,7 +45,7 @@ public class Door : MonoBehaviour
         }
         else
         {
-            // Se a porta n�o estiver aberta, retorna � sua posi��o inicial
+            // Se a porta não estiver aberta, retorna à sua posição inicial
             doorModel.position = Vector3.Lerp(doorModel.position, initialPosition, openSpeed * Time.deltaTime);
         }
     }
@@ -55,8 +56,11 @@ public class Door : MonoBehaviour
 
     public void OpenDoor()
     {
-        // Define o estado da porta como aberta
-        isOpen = true;
+        // Abre a porta somente se não estiver bloqueada
+        if (!lockedByPassword)
+        {
+            isOpen = true;
+        }
     }
 
     public void CloseDoor()
@@ -67,7 +71,7 @@ public class Door : MonoBehaviour
 
     public bool IsOpen()
     {
-        // Retorna true se a porta estiver aberta, sen�o retorna false
+        // Retorna true se a porta estiver aberta, senão retorna false
         return isOpen;
     }
 
